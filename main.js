50b069756e5e8e999536188bff6308edad39fe6a
ethan <ethanwilliams.me@gmail.com>
2022-04-17 13:46:52 -0400

    added main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('#about_button');
+about_button.addEventListener('mouseHover', (e) => {
+e.preventDefault();
+document.querySelector('#about_button').style.background = '#ccc';
+});
