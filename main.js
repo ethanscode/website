d7d40230ba6dc52bba4d0fa4bcb4fd6f61a82818
ethan <ethanwilliams.me@gmail.com>
2022-04-18 18:05:59 -0400

    validated all pages

--- main.js
+++
@@ -1,5 +0,0 @@
-const menu = document.querySelector('#about_button');
-about_button.addEventListener('mouseHover', (e) => {
-e.preventDefault();
-document.querySelector('#about_button').style.background = '#ccc';
-});
